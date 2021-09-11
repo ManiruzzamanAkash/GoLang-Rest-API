3f107027e97b4c50b9e35631ca9c82298a35f3be
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-11 12:38:19 +0600

    Added Update Book Route

--- main.go
+++ main.go
@@ -59,6 +59,26 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(book)
 }
 
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"] // Update for this ID
+			books = append(books, book)
+			json.NewEncoder(w).Encode(books)
+			return
+		}
+	}
+
+}
+
 /**
 * Main function to handle all of the routes...
  */
